38e2c9e46a3db64a78781b8f2f58ec394ce14cf5
Lily Trofimova <trofimovaliliy4@gmail.com>
2022-08-13 00:08:21 +0700

    update: index file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // your solution
+    let stack = [];
+    let open = {'(' : ')', '{' : '}', '[' : ']', '1' : '2', '3' : '4', '5' : '6', '7' : '7', '8' : '8'};
+    let close = {')':true, '}':true, ']': true, '2': true, '4': true, '6': true, '7': true, '8': true};
+    
+    
+    for (let char of str) {
+        if (open[char]) {
+            stack.push(char);
+        } else if (close[char]) {
+            if (open[stack.pop()] !== char) {
+                return false;
+            } 
+        }
+    }
+    
+    return stack.length === 0;
 }
